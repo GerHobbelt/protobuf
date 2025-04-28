5edff847def9361cf75c13462a36e348fcd43af7
Richard Shepherd <rshepherd@esri.com>
2025-04-28 11:00:54 +0100

    reinstate needed support functions

--- protobuf.lua
+++ protobuf.lua
@@ -19,6 +19,7 @@ includedirs {
 
 files {
   "src/google/protobuf/any_lite.cc",
+  "src/google/protobuf/arena.cc",
   "src/google/protobuf/arenastring.cc",
   "src/google/protobuf/arenaz_sampler.cc",
   "src/google/protobuf/extension_set.cc",
@@ -27,13 +28,22 @@ files {
   "src/google/protobuf/generated_message_util.cc",
   "src/google/protobuf/implicit_weak_message.cc",
   "src/google/protobuf/inlined_string_field.cc",
+  "src/google/protobuf/io/coded_stream.cc",
+  "src/google/protobuf/io/io_win32.cc",
+  "src/google/protobuf/io/strtod.cc",
+  "src/google/protobuf/io/zero_copy_stream.cc",
+  "src/google/protobuf/io/zero_copy_stream_impl.cc",
+  "src/google/protobuf/io/zero_copy_stream_impl_lite.cc",
   "src/google/protobuf/map.cc",
   "src/google/protobuf/message_lite.cc",
   "src/google/protobuf/parse_context.cc",
+  "src/google/protobuf/port.cc",
   "src/google/protobuf/raw_ptr.cc",  
   "src/google/protobuf/repeated_field.cc",
   "src/google/protobuf/repeated_ptr_field.cc",
+  "src/google/protobuf/stubs/common.cc",
   "src/google/protobuf/wire_format_lite.cc",
+  "third_party/utf8_range/utf8_validity.cc",  
 }
 
 if (_PLATFORM_ANDROID) then
